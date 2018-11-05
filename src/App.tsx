e0ec311b5a2e83b09ce76d441c2fa4c6d11b194a
Ryan Burgett <thesonicdoctor@gmail.com>
2018-11-04 22:58:00 -0500

    Added Routing
    - Routing Added for Home
    - Routing Added for createAccount
    - Routing Added for resetPassword

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,43 @@
 import * as React from 'react';
 import './App.css';
 import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import CreateAccount from './containers/createAccount';
+import resetPassword from './containers/resetPassword';
+
 
 class App extends React.Component {
-  public render() {
-    return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>Welcome to McHacks</h1>
-        </header>
-      </div>
-    );
-  }
+    
+    public render() {
+      return (
+        <Router>
+          <div>
+          <Route exact={true} path="/" component={Home}/>
+          <Route path="/createAccount/" component={CreateAccount}/>
+          <Route path="/resetPassword/" component={resetPassword}/>
+          </div>
+        </Router>
+      );
+      
+    }
+  
+
 }
+const Home = () => 
+      <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <h1 className='App-title'>Welcome to McHacks</h1>
+      </header>
+      <div className="container">
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/createAccount/">createAccount</Link></li>
+          <li><Link to="/resetPassword">resetPassword</Link></li>
+        </ul>
+     <hr/>
+    </div>
+    </div>
+
 
 export default App;
