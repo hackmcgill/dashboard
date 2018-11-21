c014ef7e14c5cc41069ce1b040849760b6f197a4
Erick Zhao <erick.zhao@mail.mcgill.ca>
2018-11-21 16:47:17 -0500

    Add Switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import CreateAccount from './containers/createAccount';
 import resetPassword from './containers/resetPassword';
 import HomeComponent from './components/HomeComponent';
@@ -10,13 +10,13 @@ class App extends React.Component {
     
     public render() {
       return (
-        <Router>
-          <div>
+        <BrowserRouter>
+          <Switch>
             <Route exact={true} path="/" component={HomeComponent}/>
             <Route path="/createAccount/" component={CreateAccount}/>
             <Route path="/resetPassword/" component={resetPassword}/>
-          </div>
-        </Router>
+          </Switch>
+        </BrowserRouter>
       );
       
     }
