776fc54ab606842aad161553111257907b543cee
Ryan Burgett <thesonicdoctor@gmail.com>
2018-11-25 20:32:50 -0500

    added redirect function

--- src/containers/passwordReset.tsx
+++ src/containers/passwordReset.tsx
@@ -15,13 +15,18 @@ export interface IPasswordResetContainerState {
 class PasswordResetContainer extends React.Component<{}, IPasswordResetContainerState>{
     constructor(props: {}) {
         super(props);
+<<<<<<< HEAD
         // this.loginRedirect = this.loginRedirect.bind(this);
+=======
+
+>>>>>>> added redirect function
         this.state = {
             redirectURL: ""
         }
     }
     public render() {
         return (
+<<<<<<< HEAD
             <Container>
                 <Flex alignItems={'center'}>
                     <Flex flexWrap={'wrap'} justifyContent={'center'}>
@@ -46,7 +51,38 @@ class PasswordResetContainer extends React.Component<{}, IPasswordResetContainer
                     </Flex>
                 </Flex>
             </Container>
+=======
+        <Flex flexWrap={'wrap'}>
+        <Box width={3}>
+            <H1>
+                <h1>Password reset</h1>
+            </H1>
+        </Box>
+        <Box width={3}>
+            <H3>
+                <h3>We've sent you a link to reset your password. Check your inbox and follow the instructions there.</h3>
+            </H3>
+        </Box>
+        <Box width={3}>
+            <Image>
+                <img src={resetLogo} height={'10px'} width={'10px'}/>
+            </Image>
+        </Box>
+        </Flex>
+>>>>>>> added redirect function
         )
     }
+    private loginRedirect(): void {
+        setTimeout(function () {
+            // after 2 seconds
+            // window.location = "/";
+         }, 2000)
+    }
 }
+<<<<<<< HEAD
 export default PasswordResetContainer;
+=======
+
+
+export default PasswordResetContainer;
+>>>>>>> added redirect function
