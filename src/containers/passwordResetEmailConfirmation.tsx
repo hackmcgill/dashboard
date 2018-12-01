e5592bbd0b0172313046dc6e4878017936feb849
Pierre Theo Klein <pierre.klein@mail.mcgill.ca>
2018-12-01 17:49:29 -0500

    Remove theme provider from passwordResetContainer

--- src/containers/passwordResetEmailConfirmation.tsx
+++ src/containers/passwordResetEmailConfirmation.tsx
@@ -4,10 +4,7 @@ import Image from 'src/shared/Image';
 import { Flex, Box } from '@rebass/grid';
 import H1 from 'src/shared/H1';
 import Paragraph from 'src/shared/Paragraph';
-import Container from 'src/shared/Container'
 import Button from 'src/shared/Button';
-import { ThemeProvider } from 'styled-components';
-import theme from 'src/theme';
 import { Link } from 'react-router-dom';
 
 class PasswordResetContainer extends React.Component<{}, {}>{
@@ -16,32 +13,28 @@ class PasswordResetContainer extends React.Component<{}, {}>{
     }
     public render() {
         return (
-            <ThemeProvider theme={theme}>
-                <Container>
-                    <Flex height={'100%'} flexWrap={'wrap'} justifyContent={'center'} alignItems={'center'} flex-direction={'column'}>
-                        <Flex alignItems={'center'}>
-                            <Box>
-                                <Image src={resetLogo} height={"4rem"} padding={'2.0rem'} />
-                            </Box>
-                            <Box>
-                                <H1 color={'#F2463A'} fontSize={'45px'}>
-                                    Password reset
-                            </H1>
-                            </Box>
-                        </Flex>
-                        <Box width={1}>
-                            <Paragraph fontSize={'23px'} center={true} paddingBottom={'32px'}>
-                                We've sent you a link to reset your password. Check your inbox and follow the instructions there.
-                                </Paragraph>
-                        </Box>
-                        <Box>
-                            <Link to="/login">
-                                <Button type='button'>Take me home!</Button>
-                            </Link>
-                        </Box>
-                    </Flex>
-                </Container>
-            </ThemeProvider>
+            <Flex justifyContent={'center'} alignItems={'center'} flexDirection={'column'}>
+                <Flex alignItems={'center'}>
+                    <Box>
+                        <Image src={resetLogo} height={"4rem"} padding={'2.0rem'} />
+                    </Box>
+                    <Box>
+                        <H1 color={'#F2463A'} fontSize={'45px'}>
+                            Password reset
+                                </H1>
+                    </Box>
+                </Flex>
+                <Box width={1}>
+                    <Paragraph fontSize={'23px'} center={true} paddingBottom={'32px'}>
+                        We've sent you a link to reset your password. Check your inbox and follow the instructions there.
+                    </Paragraph>
+                </Box>
+                <Box>
+                    <Link to="/login">
+                        <Button type='button'>Take me home!</Button>
+                    </Link>
+                </Box>
+            </Flex>
         )
     }
 }
