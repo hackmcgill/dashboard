70d5f4f09704b4d828f3d4f1863f2367cac1e980
Erick Zhao <erick.zhao@mail.mcgill.ca>
2018-12-02 18:30:13 -0500

    Make H2 component

---
+++ src/shared/H2.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+import { ITheme } from 'src/theme';
+
+interface IH2Props {
+    color?: string;
+    fontSize?: string;
+    textAlign?: string;
+    theme?: ITheme
+}
+
+const H2 = styled.h2`
+font-size: ${(props: IH2Props) => props.fontSize ? props.fontSize : '36px'};
+text-align: ${(props: IH2Props) => props.textAlign ? props.textAlign : 'center'};
+color: ${(props: IH2Props) => props.color ? props.color : props.theme && props.theme.primary || 'red'};
+`
+export default H2;
