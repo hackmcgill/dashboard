093d359446006d561eba5ec16e36e87598d84f9b
Pierre Theo Klein <pierre.klein@mail.mcgill.ca>
2018-12-02 12:59:55 -0500

    Finish styling for textarea, prelim styling for checkbox

--- src/shared/Textarea.tsx
+++ src/shared/Textarea.tsx
@@ -1,3 +1,16 @@
 import styled from 'styled-components';
-const Textarea = styled.textarea``
+const Textarea = styled.textarea`
+    border-radius: 20px;
+    border: 2px solid ${props => props.theme.greyLight};
+    box-sizing: border-box;
+    display: block;
+    font-size: 16px;
+    margin: auto;
+    margin-top: 10px;
+    margin-bottom: 20px;
+    min-height: 200px;
+    padding-left: 16px;
+    padding-right: 16px;
+    width: 80%;
+`
 export default Textarea; 
