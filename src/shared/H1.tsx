3d466adf960a4edf3325248daf641727c7ead543
Erick Zhao <erick.zhao@mail.mcgill.ca>
2018-12-02 18:30:08 -0500

    Add theme prop to H1 component

--- src/shared/H1.tsx
+++ src/shared/H1.tsx
@@ -1,15 +1,16 @@
 import styled from 'styled-components';
+import { ITheme } from 'src/theme';
 
 interface IH1Props {
     color?: string;
     fontSize?: string;
     textAlign?: string;
+    theme?: ITheme
 }
 
 const H1 = styled.h1`
 font-size: ${(props: IH1Props) => props.fontSize ? props.fontSize : '36px'};
 text-align: ${(props: IH1Props) => props.textAlign ? props.textAlign : 'center'};
-color: ${(props: IH1Props) => props.color ? props.color : '#F2463A'};
+color: ${(props: IH1Props) => props.color ? props.color : props.theme && props.theme.primary || 'red'};
 `
 export default H1;
-
