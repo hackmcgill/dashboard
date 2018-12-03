09d02ec0658aa15b395e36cf5973b0f7a5b43f2e
Erick Zhao <erick.zhao@mail.mcgill.ca>
2018-12-03 15:11:08 -0500

    Get hacker info

--- src/containers/dashboard.tsx
+++ src/containers/dashboard.tsx
@@ -1,31 +1,52 @@
-    import * as React from "react";
-    import Card from "src/shared/Card";
-    import { Flex, Box } from "@rebass/grid";
-    import iconAccount from "src/assets/images/dashboard-account.svg";
-    import iconApplication from "src/assets/images/dashboard-application.svg";
-    // import iconTeam from 'src/assets/images/dashboard-team.svg';
-    import BackgroundLandscape from "src/assets/images/backgroundLandscape.svg";
-    import H2 from "src/shared/H2";
-    import Image from "src/shared/Image";
-    import { Link } from "react-router-dom";
-    import HackerStatus from "src/config/hackerStatus";
-    import BackgroundImage from "src/shared/BackgroundImage";
+import * as React from "react";
+import Card from "src/shared/Card";
+import { Flex, Box } from "@rebass/grid";
+import iconAccount from "src/assets/images/dashboard-account.svg";
+import iconApplication from "src/assets/images/dashboard-application.svg";
+// import iconTeam from 'src/assets/images/dashboard-team.svg';
+import BackgroundLandscape from "src/assets/images/backgroundLandscape.svg";
+import H2 from "src/shared/H2";
+import Image from "src/shared/Image";
+import { Link } from "react-router-dom";
+import HackerStatus from "src/config/hackerStatus";
+import BackgroundImage from "src/shared/BackgroundImage";
 
-    import MediaQuery from 'react-responsive';
+import MediaQuery from 'react-responsive';
+import hacker from 'src/api/hacker';
+import H1 from 'src/shared/H1';
+import { IHacker } from 'src/config/userTypes';
 
-    export interface IDashboardState {
+export interface IDashboardState {
     status: HackerStatus;
-    }
+}
 
-    /**
-     * Container that renders form to log in.
-     */
-    class DashboardContainer extends React.Component<{}, IDashboardState> {
-    constructor(props: {}) {
-        super(props);
+/**
+ * Container that renders form to log in.
+ */
+class DashboardContainer extends React.Component<{}, IDashboardState> {
+constructor(props: {}) {
+    super(props);
+    this.state = {
+        status: HackerStatus.HACKER_STATUS_NONE
     }
-    public render() {
-        return (
+}
+
+public async componentDidMount() {
+    try {
+        const response = await hacker.getSelf();
+        saveHackerInfo(response.data);
+    } catch(e) {
+        if (e.status === 401) {
+            this.setState({status: HackerStatus.HACKER_STATUS_NONE})
+        }
+    }
+}
+
+public render() {
+    const { status } = this.state;
+    return (
+        <Flex flexDirection={'column'} alignItems={'center'}>
+            <H1>status: {status.toLowerCase()}</H1>
             <Flex flexWrap={"wrap"} alignItems={"center"} justifyContent={"center"}>
                 <Link to="/">
                 <Card width={"250px"} flexDirection={"column"}>
@@ -46,9 +67,16 @@
                     </Box>
                 </MediaQuery>
             </Flex>
-        
-        );
-    }
-    }
 
-    export default DashboardContainer;
+        </Flex>
+        
+    
+    );
+}
+}
+
+function saveHackerInfo(info: IHacker) {
+    console.log(info);
+}
+
+export default DashboardContainer;
