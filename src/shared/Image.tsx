006e919cb63e9f7d3f68118c4bc7d689b3036d27
Pierre Theo Klein <pierre.klein@mail.mcgill.ca>
2018-12-03 18:32:33 -0500

    Update image prop

--- src/shared/Image.tsx
+++ src/shared/Image.tsx
@@ -2,12 +2,12 @@ import styled from 'styled-components';
 
 export interface IImageProps {
     src: any;
-    height?: string;
+    imgHeight?: string;
     padding?: string;
 }
 const Image = styled.img`
     src: ${(props: IImageProps) => props.src};
-    height: ${(props: IImageProps) => props.height || 'auto'};
+    height: ${(props: IImageProps) => props.imgHeight || 'auto'};
     width: auto;
     padding: ${(props: IImageProps) => props.padding || 0};
 `
