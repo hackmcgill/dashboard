65acddb79c5cc33cfa553480eb8c19c7ae4c20a9
Pierre Theo Klein <pierre.klein@mail.mcgill.ca>
2018-12-02 19:46:38 -0500

    Add frontend account confirm route

--- src/config/FrontendRoute.ts
+++ src/config/FrontendRoute.ts
@@ -1,5 +1,6 @@
 enum FrontendRoute {
     CREATE_ACCOUNT_PAGE = '/account/create',
+    CONFIRM_ACCOUNT_PAGE = '/account/confirm',
     FORGOT_PASSWORD_PAGE = '/password/forgot',
     RESET_PASSWORD_PAGE = '/password/reset',
     LOGIN_PAGE = '/login',
