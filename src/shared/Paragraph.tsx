deb25188556f6cba98a44935773f501cae904f5c
Pierre Theo Klein <pierre.klein@mail.mcgill.ca>
2018-12-03 21:13:40 -0500

    Fix more PR comments

--- src/shared/Paragraph.tsx
+++ src/shared/Paragraph.tsx
@@ -13,7 +13,7 @@ export interface IParagraphProps {
 const Paragraph = styled.p<IParagraphProps>`
     font-size: ${props => props.fontSize ? props.fontSize : '24px'};
     color: ${props => props.color ? props.color : props.theme.grey};
-    text-align: ${props => props.textAlign ? props.textAlign : 'center'};
+    text-align: ${props => props.textAlign ? props.textAlign : 'left'};
     padding-bottom: ${(props: IParagraphProps) => props.paddingBottom ? props.paddingBottom : '0px'};
     max-width: ${(props: IParagraphProps) => props.maxWidth ? props.maxWidth : '800px'};
     margin-bottom: ${(props: IParagraphProps) => props.marginBottom ? props.marginBottom : '18px'};
