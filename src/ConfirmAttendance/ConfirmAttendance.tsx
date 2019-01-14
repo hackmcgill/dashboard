cb2ef2441e4dd0ad899adfa8024d724d864d0f35
Pierre Theo Klein <pierre.klein@mail.mcgill.ca>
2019-01-13 19:31:52 -0500

    Allow user to decline

--- src/ConfirmAttendance/ConfirmAttendance.tsx
+++ src/ConfirmAttendance/ConfirmAttendance.tsx
@@ -14,7 +14,8 @@ import WithToasterContainer from '../shared/HOC/withToaster';
 import theme from '../shared/Styles/theme';
 
 interface IConfirmAttendanceState {
-  isConfirmed: boolean;
+  submitted: boolean;
+  submissionBtn: number;
 }
 
 class ConfirmAttendanceContainer extends React.Component<
@@ -24,12 +25,15 @@ class ConfirmAttendanceContainer extends React.Component<
   constructor(props: {}) {
     super(props);
     this.state = {
-      isConfirmed: false,
+      submissionBtn: 0,
+      submitted: false,
     };
     this.onSubmit = this.onSubmit.bind(this);
+    this.renderFormik = this.renderFormik.bind(this);
+    this.onClickFactory = this.onClickFactory.bind(this);
   }
   public render() {
-    if (this.state.isConfirmed) {
+    if (this.state.submitted) {
       return <Redirect to={FrontendRoute.HOME_PAGE} />;
     }
     return (
@@ -88,14 +92,24 @@ class ConfirmAttendanceContainer extends React.Component<
           required={true}
         />
         <ErrorMessage component={FormikElements.Error} name="liability" />
-        <Flex justifyContent={'center'}>
+        <Flex justifyContent={'center'} mb={'20px'}>
           <Box>
             <Button
-              type="submit"
+              onClick={this.onClickFactory(0, fp.submitForm)}
+              secondary={true}
               isLoading={fp.isSubmitting}
               disabled={fp.isSubmitting}
             >
-              Confirm my Attendance
+              Decline
+            </Button>
+          </Box>
+          <Box>
+            <Button
+              onClick={this.onClickFactory(1, fp.submitForm)}
+              isLoading={fp.isSubmitting}
+              disabled={fp.isSubmitting}
+            >
+              Confirm
             </Button>
           </Box>
         </Flex>
@@ -103,11 +117,28 @@ class ConfirmAttendanceContainer extends React.Component<
     );
   }
 
+  private onClickFactory(
+    submissionBtn: number,
+    submitForm: () => void
+  ): (e: any) => void {
+    return (e) => {
+      this.setState({ submissionBtn });
+      submitForm();
+    };
+  }
+
   private async onSubmit() {
     try {
       const hackerId = (await Hacker.getSelf()).data.data.id;
-      await Hacker.confirm(hackerId, true);
-      this.setState({ isConfirmed: true });
+      switch (this.state.submissionBtn) {
+        case 0:
+          await Hacker.confirm(hackerId, false);
+          break;
+        case 1:
+          await Hacker.confirm(hackerId, true);
+          break;
+      }
+      this.setState({ submitted: true });
     } catch (e) {
       ValidationErrorGenerator(e.data);
     }
