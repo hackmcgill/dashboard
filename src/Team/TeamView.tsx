93ebde67a2c3eade7b78ec572ec5d07731726cf5
Pierre Theo Klein <pierre.klein@mail.mcgill.ca>
2019-01-15 17:31:52 -0500

    Add toaster notifications for clipboard component

--- src/Team/TeamView.tsx
+++ src/Team/TeamView.tsx
@@ -14,13 +14,15 @@ import {
 } from '../config';
 
 import ClipboardComponent from '../shared/Elements/Clipboard';
+import WithToasterContainer from '../shared/HOC/withToaster';
+import { toast } from 'react-toastify';
 
 interface ITeamViewProps {
   team: ITeam;
   members: IMemberName[];
 }
 
-export const TeamView: React.StatelessComponent<ITeamViewProps> = (props) => {
+const TV: React.StatelessComponent<ITeamViewProps> = (props) => {
   const firstColumnWidth = 0.5;
   const nameList = props.members.map((member, index) => (
     <Box key={index}>{member.firstName}</Box>
@@ -28,13 +30,17 @@ export const TeamView: React.StatelessComponent<ITeamViewProps> = (props) => {
   return (
     <Flex flexDirection={'column'}>
       <Box>
-        <Flex justifyContent={'space-between'}>
+        <Flex justifyContent={'space-between'} alignItems={'flex-start'}>
           <Box width={firstColumnWidth}>
             <Bold>{TEAM_NAME}</Bold>
             <Box fontSize={'smaller'}>{TEAM_NAME_SUBTITLE}</Box>
           </Box>
           <Box>
-            <ClipboardComponent value={props.team.name} />
+            <ClipboardComponent
+              value={props.team.name}
+              onSuccess={onCopied}
+              onError={onCopyFailed}
+            />
           </Box>
         </Flex>
       </Box>
@@ -52,3 +58,12 @@ export const TeamView: React.StatelessComponent<ITeamViewProps> = (props) => {
     </Flex>
   );
 };
+
+function onCopied(e: any) {
+  toast.success('Copied!');
+}
+function onCopyFailed(e: any) {
+  toast.error('Error.');
+}
+
+export const TeamView = WithToasterContainer(TV);
