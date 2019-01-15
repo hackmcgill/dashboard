46810c173eb10823d6c7aaeab2fca7d27f3f04ff
Erick Zhao <erick.zhao@mail.mcgill.ca>
2019-01-15 00:28:59 -0500

    Remove leftover debug text

--- src/Search/ResultsTable.tsx
+++ src/Search/ResultsTable.tsx
@@ -43,7 +43,6 @@ const ResultsTable: React.StatelessComponent<IResultsTableProps> = (props) => {
       Header: 'Applicant Info',
       Cell: ({ original }: any) => (
         <div>
-          <span>{original.hacker.accountId.firstName}</span>
           <SingleHackerModal
             hacker={original.hacker}
             allHackers={props.results.map((r) => r.hacker)}
