829b0c6c1138f22f1896b1697ba6cbba4591d582
Richard Zhang <richardzhang97@gmail.com>
2019-01-19 16:57:46 -0500

    Add front end route

--- src/config/frontendRoutes.ts
+++ src/config/frontendRoutes.ts
@@ -13,6 +13,8 @@ export enum FrontendRoute {
   RESET_PASSWORD_PAGE = '/password/reset',
   SPONSOR_SEARCH_PAGE = '/sponsor/search',
   TEAM_PAGE = '/team',
+
+  CREATE_SPONSOR_PAGE = '/sponsor/create',
 }
 
 export default FrontendRoute;
