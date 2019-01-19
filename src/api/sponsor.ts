58ba012d0f0b039f0d65881c0dc027cedb1d938e
Richard Zhang <richardzhang97@gmail.com>
2019-01-19 16:56:19 -0500

    Add the actual getSelf endpoint

--- src/api/sponsor.ts
+++ src/api/sponsor.ts
@@ -1,9 +1,12 @@
-import { AxiosPromise } from 'axios';
-import { APIRoute, ISponsor } from '../config';
+import { AxiosPromise, AxiosResponse } from 'axios';
+import { APIRoute, ISponsor, CACHE_SPONSOR_KEY } from '../config';
 import API from './api';
+import { APIResponse } from '.';
+import LocalCache from '../util/LocalCache';
 class SponsorAPI {
   constructor() {
     API.createEntity(APIRoute.SPONSOR);
+    API.createEntity(APIRoute.SPONSOR_SELF);
   }
   /**
    * Create an account.
@@ -11,6 +14,20 @@ class SponsorAPI {
    */
   public create(sponsor: ISponsor): AxiosPromise {
     return API.getEndpoint(APIRoute.SPONSOR).create(sponsor);
+  }
+  /**
+   * Get the logged-in user's sponsor information, if they have a sponsor info.
+   */
+  public async getSelf(
+    overrideCache?: boolean
+  ): Promise<AxiosResponse<APIResponse<ISponsor>>> {
+    const cached: any = LocalCache.get(CACHE_SPONSOR_KEY);
+    if (cached && !overrideCache) {
+      return cached as AxiosResponse<APIResponse<ISponsor>>;
+    }
+    const value = await API.getEndpoint(APIRoute.SPONSOR_SELF).getAll();
+    LocalCache.set(CACHE_SPONSOR_KEY, value);
+    return value;
   }
   /**
    * Get information about a sponsor
