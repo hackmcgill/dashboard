553ef76ab74f38b03eed2d29900ad775a6215fa0
alinastarkov <hayleeluu@gmail.com>
2019-11-24 17:38:46 -0500

    added props for side bar menu item

---
+++ src/shared/Elements/SideBarMenuItem.tsx
@@ -0,0 +1,33 @@
+import styled from '../Styles/styled-components';
+
+interface ISideBarMenuItemProps {
+  currentPage: boolean;
+}
+
+export const SideBarMenuItem = styled.div<ISideBarMenuItemProps>`
+  padding: 1.5rem 0 1rem 2rem;
+  display: flex;
+  background-color: ${(props) => (props.currentPage ? '#F2463A' : '')};
+
+  img {
+    flex-direction: column;
+    height: 2rem;
+    width: 2rem;
+  }
+
+  h2 {
+    padding-left: 35px;
+    padding-top: 2px;
+    text-decoration: none;
+    transition: color 0.3s linear;
+
+    @media (max-width: 576px) {
+      font-size: 1.5rem;
+      text-align: center;
+    }
+
+    &:hover {
+      color: #ffff;
+    }
+  }
+`;
