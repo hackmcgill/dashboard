4083d2e539d109f40ffca00620870af084f85ded
Haylee Luu <hayleeluu@gmail.com>
2019-11-25 16:30:12 -0500

    Feature/88 sidebar ver2 (#728)
    * refactor Dashboard texts
    
    * added background illustration
    
    * added display props to h1
    
    * added display props to H2
    
    * change button border-radius
    
    * added styled status page
    
    * added status page to main page for hacker
    
    * Update src/features/Dashboard/Main.tsx
    
    Co-Authored-By: Loreina Chew <loreina.chew@gmail.com>
    
    * Update src/features/Dashboard/Main.tsx
    
    Co-Authored-By: Loreina Chew <loreina.chew@gmail.com>
    
    * added sidebar icon
    
    * added SideBarMenuItem styled component
    
    * Added SideBar styled-component
    
    * added SideBar
    
    * export SideBar
    
    * added SideBar in StatusPage
    
    * added  pagetype in config
    
    * refactor sidebar, highlight current page
    
    * change padding top
    
    * added props for side bar menu item
    
    * change extension from jsx to tsx
    
    * remove status page from dashboard
    
    * remove status page  to prevent conflicts
    
    * remove hover over text in sidebar
    
    * 100% height sidebar

---
+++ src/features/Dashboard/SideBar.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import { Image, H2, SideBar, SideBarMenuItem } from '../../shared/Elements';
+
+import AppIcon from '../../assets/images/sidebar-app.svg';
+import BusIcon from '../../assets/images/sidebar-bus.svg';
+import HomeIcon from '../../assets/images/sidebar-home.svg';
+import ProfileIcon from '../../assets/images/sidebar-profile.svg';
+import TeamIcon from '../../assets/images/sidebar-team.svg';
+import { PageType } from '../../config';
+
+interface ISideBarProps {
+  currentPage: PageType;
+}
+
+export const SideBarComponent: React.SFC<ISideBarProps> = (props) => {
+  const PageTypeObj: any = {
+    Home: HomeIcon,
+    Application: AppIcon,
+    Profile: ProfileIcon,
+    Team: TeamIcon,
+    Travel: BusIcon,
+  };
+
+  const whiteIcon = {
+    filter:
+      'invert(97%) sepia(100%) saturate(12%) hue-rotate(248deg) contrast(104%) brightness(100) saturate(100%)',
+  };
+
+  return (
+    <SideBar>
+      {Object.keys(PageTypeObj).map((type) => (
+        <SideBarMenuItem
+          currentPage={props.currentPage === type ? true : false}
+        >
+          <Image
+            src={PageTypeObj[type]}
+            style={props.currentPage === type ? whiteIcon : undefined}
+          />
+          <H2 color={props.currentPage === type ? 'white' : '#4d4d4d'}>
+            {type}
+          </H2>
+        </SideBarMenuItem>
+      ))}
+    </SideBar>
+  );
+};
---
+++ src/shared/Elements/Sidebar.tsx
@@ -0,0 +1,18 @@
+import styled from '../Styles/styled-components';
+
+export const SideBar = styled.nav`
+  display: flex;
+  flex-direction: column;
+  background: #f4f4f4;
+  transform: 'translateX(-100%)';
+  height: 100%;
+  width: 18%;
+  text-align: left;
+  padding: 3rem 0 0 0;
+  position: absolute;
+  left: 0;
+
+  @media (max-width: 576px) {
+    width: 100%;
+  }
+`;
