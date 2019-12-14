f8eff178a4207568e8b755e36eabffa85dbf1732
Loreina Chew <loreina.chew@gmail.com>
2019-12-14 18:00:41 -0500

    alphabetize ethnicities

--- src/config/ethnicity.ts
+++ src/config/ethnicity.ts
@@ -1,10 +1,10 @@
 export enum IEthnicity {
-  AFRO_AMER = 'Black or African American',
   ASIAN_PI = ' Asian or Pacific Islander',
-  EUROPEAN = 'White or Caucasian',
+  AFRO_AMER = 'Black or African American',
   HISP = 'Hispanic',
-  MID_EAST = 'Middle Eastern',
   NATIVE_AM = 'Indigenous or Inuit',
+  MID_EAST = 'Middle Eastern',
+  EUROPEAN = 'White or Caucasian',
   NO_ANS = 'Prefer not to answer',
 }
 export default IEthnicity;
