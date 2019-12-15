509b4911d569e61dd04c095ad79dc1653ffccac0
Loreina Chew <loreina.chew@gmail.com>
2019-12-14 22:39:03 -0500

    Update fields (#744)
    * shorten pronoun list
    
    * alphabetize genders
    
    * update degrees
    
    * alphabetize dietary restrictions with none on top
    
    * update grad years list
    
    * alphabetize job interests
    
    * fix job interests
    
    * update majors
    
    * update skills
    
    * update changelog
    
    * alphabetize ethnicities
    
    * change pronouns case
    
    * change case for skills
    
    * Update ethnicities
    
    * Make no answer consistently "prefer not to answer"
    
    * add more majors
    
    * add more majors

--- src/config/ethnicity.ts
+++ src/config/ethnicity.ts
@@ -1,10 +1,11 @@
 export enum IEthnicity {
-  AFRO_AMER = 'Black or African American',
-  ASIAN_PI = ' Asian or Pacific Islander',
-  EUROPEAN = 'White or Caucasian',
+  INDIAN_NATIVE = 'American Indian / Alaskan Native',
+  ASIAN_PI = ' Asian / Pacific Islander',
+  AFRO_AMER = 'Black / African American',
+  INDIGENOUS_INUIT = 'Indigenous / Inuit',
   HISP = 'Hispanic',
   MID_EAST = 'Middle Eastern',
-  NATIVE_AM = 'Indigenous or Inuit',
-  NO_ANS = 'Prefer not to answer',
+  EUROPEAN = 'White / Caucasian',
+  NO_ANSWER = 'Prefer not to answer',
 }
 export default IEthnicity;
--- src/config/gradYears.ts
+++ src/config/gradYears.ts
@@ -1,2 +1,2 @@
-const GradYearList = [2018, 2019, 2020, 2021, 2022];
+const GradYearList = [2019, 2020, 2021, 2022, 2023];
 export const GradYears = GradYearList.map((v) => ({ label: v, value: v }));
