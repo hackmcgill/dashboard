6c2789ea1c541a0218e52e900c8d7e4bd6cc8596
RohitGarudadri <rohit.garudadri@mail.mcgill.ca>
2019-12-15 17:08:55 -0500

    Using hamburger styled sidebar attached to navbar

--- src/features/Nav/Navbar.tsx
+++ src/features/Nav/Navbar.tsx
@@ -1,12 +1,17 @@
 import { Box, Flex } from '@rebass/grid';
 import * as React from 'react';
 
+import { slide as Menu } from 'react-burger-menu';
 import Martlett from '../../assets/images/mchacks-martlet-tight.svg';
 import { FrontendRoute } from '../../config/frontendRoutes';
 import { Image } from '../../shared/Elements';
 import { isLoggedIn } from '../../util/UserInfoHelperFunctions';
+import MobileMenu from '../Sidebar/MobileMenu';
 import LogoutBtn from './LogoutButton';
 import Nav from './Nav';
+// import SidebarItem from '../Sidebar/SidebarItem';
+// import theme from '../../shared/Styles/theme';
+import LoginBtn from './LoginButton';
 
 interface INavbarState {
   loggedIn: boolean;
@@ -20,8 +25,29 @@ export default class Navbar extends React.Component<{}, INavbarState> {
     };
     this.checkLoggedIn();
   }
+
   public render() {
-    const logoutBtn = this.state.loggedIn ? <LogoutBtn /> : '';
+    const tabs = this.getTabs();
+    // need to have burgerBuns instead of logoutBtn now. logout will be inside burger
+    // const logoutBtn = this.state.loggedIn ? <LogoutBtn /> : '';
+    // const applicationBtn = (
+    //   <LinkDuo
+    //     to={FrontendRoute.EDIT_APPLICATION_PAGE}
+    //     key={'Application'}
+    //     style={{
+    //       textDecoration: 'none',
+    //       overflow: 'scroll',
+    //     }}
+    //   >
+    //     <SidebarItem
+    //       currentPage={true}
+    //       title={'Application'}
+    //       hidden={!this.state.loggedIn}
+    //     >
+    //       <H2 color={theme.colors.black80}>{'Application'}</H2>
+    //     </SidebarItem>
+    //   </LinkDuo>
+    // );
     return (
       <Nav borderThickness={'1px'}>
         <Flex flexDirection={'row'} justifyContent={'space-between'} p={'1rem'}>
@@ -30,11 +56,31 @@ export default class Navbar extends React.Component<{}, INavbarState> {
               <Image src={Martlett} padding={'0rem'} />
             </a>
           </Box>
-          <Box alignSelf={'center'}>{logoutBtn}</Box>
+          <Menu isOpen={true} styles={MobileMenu}>
+            {tabs[0]}
+          </Menu>
         </Flex>
       </Nav>
     );
   }
+
+  private getTabs() {
+    const tabs = [];
+    if (this.state.loggedIn) {
+      // tabs.push(<LogoutBtn />);
+      // tabs.push(<ApplicationBtn />);
+      // tabs.push(<HomeBtn />);
+      // tabs.push(<ProfileBtn />);
+      // tabs.push(<TravelBtn/>);
+      // ^Have to create these buttons! With proper redirects if they have
+      // created/confirmed accounts and to created/edit their application, etc.
+    } else {
+      tabs.push(<LoginBtn />);
+    }
+
+    return tabs;
+  }
+
   private checkLoggedIn() {
     isLoggedIn().then((result) => {
       this.setState({
---
+++ src/features/Sidebar/MobileMenu.jsx
@@ -0,0 +1,65 @@
+import theme from '../../shared/Styles/theme';
+export const MobileMenu = {
+  bmBurgerButton: {
+    position: 'fixed',
+    width: '30px',
+    height: '25px',
+    top: '22px',
+    right: '30px',
+  },
+  bmBurgerBars: {
+    background: theme.colors.black40,
+    borderRadius: '30px',
+    height: '3.5px',
+  },
+  bmBurgerBarsHover: {
+    background: theme.colors.black60,
+  },
+  bmCrossButton: {
+    height: '30px',
+    width: '30px',
+    top: '22px',
+    right: '30px',
+    outline: 'none',
+  },
+  bmCross: {
+    background: theme.colors.black40,
+    height: '5px',
+    width: '30px',
+    top: '5px',
+    left: '-15px',
+    borderRadius: '5px',
+  },
+  bmMenuWrap: {
+    position: 'fixed',
+    height: '100%',
+  },
+  bmMenu: {
+    background: theme.colors.black5,
+    padding: '2.5em 1.5em 0',
+  },
+  bmMorphShape: {
+    fill: '#373a47',
+  },
+  bmItemList: {
+    color: theme.colors.red,
+    padding: '0.8em',
+    top: '8em',
+    display: 'grid',
+    height: '250px',
+    textAlign: 'center',
+    fontSize: '24px',
+    marginTop: '60px',
+  },
+  bmItem: {
+    textAlign: 'center',
+    lineHeight: '7rem',
+    fontDize: '36px',
+    padding: '40px',
+  },
+  bmOverlay: {
+    background: 'rgba(0, 0, 0, 0.3)',
+  },
+};
+
+export default MobileMenu;
