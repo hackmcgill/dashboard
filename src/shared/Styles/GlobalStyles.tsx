c6e902e6c46a13cb25aa75c50b1f24288abd4730
Loreina Chew <loreina.chew@gmail.com>
2019-12-16 13:11:05 -0500

    update global styles

--- src/shared/Styles/GlobalStyles.tsx
+++ src/shared/Styles/GlobalStyles.tsx
@@ -50,7 +50,7 @@ export const GlobalStyles = createGlobalStyle`
     z-index: 100000;
   }
 
-  @media screen and (min-width: 768px) {
+  @media screen and (min-width: ${(props) => props.theme.screens.smUp}) {
     .bm-burger-button, .bm-menu-wrap {
         display: none;
     }
