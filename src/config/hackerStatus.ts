1cecbb1a8672b81499daf6b0a52ff9b0f493d510
Loreina Chew <loreina.chew@gmail.com>
2019-12-19 21:57:43 -0500

    change cancelled -> withdrawn status

--- src/config/hackerStatus.ts
+++ src/config/hackerStatus.ts
@@ -5,7 +5,7 @@ export enum HackerStatus {
   HACKER_STATUS_DECLINED = 'Declined',
   HACKER_STATUS_WAITLISTED = 'Waitlisted',
   HACKER_STATUS_CONFIRMED = 'Confirmed',
-  HACKER_STATUS_CANCELLED = 'Cancelled',
+  HACKER_STATUS_WITHDRAWN = 'Withdrawn',
   HACKER_STATUS_CHECKED_IN = 'Checked-in',
 }
 export default HackerStatus;
