78d5b73b1ef90744368ea47f8182915962bdfe3d
Loreina Chew <loreina.chew@gmail.com>
2019-12-19 21:57:35 -0500

    add declined status

--- src/config/hackerStatus.ts
+++ src/config/hackerStatus.ts
@@ -2,6 +2,7 @@ export enum HackerStatus {
   HACKER_STATUS_NONE = 'None',
   HACKER_STATUS_APPLIED = 'Applied',
   HACKER_STATUS_ACCEPTED = 'Accepted',
+  HACKER_STATUS_DECLINED = 'Declined',
   HACKER_STATUS_WAITLISTED = 'Waitlisted',
   HACKER_STATUS_CONFIRMED = 'Confirmed',
   HACKER_STATUS_CANCELLED = 'Cancelled',
