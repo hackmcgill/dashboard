43756266e8a938f830184c58573c2692de5534b4
logan-r <logan.ralston@mail.mcgill.ca>
2020-07-25 23:00:08 -0400

    feat: improve responsive background images on accounts pages

--- src/pages/Account/Create.tsx
+++ src/pages/Account/Create.tsx
@@ -8,37 +8,78 @@ import {
   H1,
   MaxWidthBox,
 } from '../../shared/Elements';
+import MediaQuery from 'react-responsive';
 
 import Coders from '../../assets/images/coders.svg';
 
 const CreateAccountPage: React.FC = () => (
-  <MaxWidthBox paddingLeft={'100px'} paddingRight={'50px'} maxWidth={'500px'}>
-    <>
-      <BackgroundImage
-        src={Coders}
-        top={'60px'}
-        right={'0px'}
-        imgHeight={'100%'}
-      />
-    </>
+  <>
     <Helmet>
       <title>
         Create Account | {CONSTANTS.HACKATHON_NAME}
       </title>
     </Helmet>
-    <H1
-      fontSize={'30px'}
-      textAlign={'left'}
-      marginTop={'0px'}
-      marginBottom={'20px'}
-      marginLeft={'0px'}
-      paddingBottom={'20px'}
-      paddingTop={'70px'}
-    >
-      Your Account
-    </H1>
-    <ManageAccountContainer mode={ManageAccountModes.CREATE} />
-  </MaxWidthBox>
+
+    <MediaQuery maxWidth="991px">
+      <MaxWidthBox m={'auto'} paddingLeft={'50px'} paddingRight={'50px'} maxWidth={'500px'}>
+        <H1
+          fontSize={'30px'}
+          textAlign={'left'}
+          marginTop={'0px'}
+          marginBottom={'20px'}
+          marginLeft={'0px'}
+          paddingBottom={'20px'}
+          paddingTop={'70px'}
+        >
+          Your Account
+        </H1>
+        <ManageAccountContainer mode={ManageAccountModes.CREATE} />
+      </MaxWidthBox>
+    </MediaQuery>
+
+    <MediaQuery minWidth="992px">
+      <MaxWidthBox paddingLeft={'100px'} paddingRight={'50px'} maxWidth={'500px'}>
+        <H1
+          fontSize={'30px'}
+          textAlign={'left'}
+          marginTop={'0px'}
+          marginBottom={'20px'}
+          marginLeft={'0px'}
+          paddingBottom={'20px'}
+          paddingTop={'70px'}
+        >
+          Your Account
+        </H1>
+        <ManageAccountContainer mode={ManageAccountModes.CREATE} />
+      </MaxWidthBox>
+    </MediaQuery>
+
+    <MediaQuery minWidth="992px" maxWidth="1093px">
+      <BackgroundImage
+        src={Coders}
+        top={'60px'}
+        right={'0px'}
+        imgHeight={'70%'}
+      />
+    </MediaQuery>
+    <MediaQuery minWidth="1094px" maxWidth="1199px">
+      <BackgroundImage
+        src={Coders}
+        top={'60px'}
+        right={'0px'}
+        imgHeight={'80%'}
+      />
+    </MediaQuery>
+    <MediaQuery minWidth="1200px">
+      <BackgroundImage
+        src={Coders}
+        top={'60px'}
+        right={'0px'}
+        imgHeight={'90%'}
+        position={'fixed' as 'fixed'}
+      />
+    </MediaQuery>
+  </>
 );
 
 export default CreateAccountPage;
--- src/pages/Account/Edit.tsx
+++ src/pages/Account/Edit.tsx
@@ -11,28 +11,17 @@ import {
 
 import Bulby from '../../assets/images/bulby.svg';
 import Drone from '../../assets/images/drone.svg';
+import MediaQuery from 'react-responsive';
+import theme from '../../shared/Styles/theme';
 
 const EditAccountPage: React.FC = () => (
   <MaxWidthBox m={'auto'} maxWidth={'500px'}>
-    <BackgroundImage
-      right={'10%'}
-      top={'178px'}
-      src={Drone}
-      imgHeight={'133px'}
-      position={'fixed' as 'fixed'}
-    />
-    <BackgroundImage
-      left={'5%'}
-      bottom={'5%'}
-      src={Bulby}
-      imgHeight={'290px'}
-      position={'fixed' as 'fixed'}
-    />
     <Helmet>
       <title>
         Edit Profile | {CONSTANTS.HACKATHON_NAME}
       </title>
     </Helmet>
+
     <H1
       fontSize={'30px'}
       textAlign={'left'}
@@ -45,6 +34,50 @@ const EditAccountPage: React.FC = () => (
       Your Account
     </H1>
     <ManageAccountContainer mode={ManageAccountModes.EDIT} />
+
+    <MediaQuery minWidth={theme.screens.smUp} maxWidth={'986px'}>
+      <BackgroundImage
+        right={'2%'}
+        top={'178px'}
+        src={Drone}
+        imgHeight={'99px'}
+        position={'fixed' as 'fixed'}
+      />
+    </MediaQuery>
+
+    <MediaQuery minWidth={'987px'} maxWidth={'1199px'}>
+      <BackgroundImage
+        right={'5%'}
+        top={'150px'}
+        src={Drone}
+        imgHeight={'133px'}
+        position={'fixed' as 'fixed'}
+      />
+      <BackgroundImage
+        left={'2%'}
+        bottom={'2%'}
+        src={Bulby}
+        imgHeight={'190px'}
+        position={'fixed' as 'fixed'}
+      />
+    </MediaQuery>
+
+    <MediaQuery minWidth={'1200px'}>
+      <BackgroundImage
+        right={'10%'}
+        top={'178px'}
+        src={Drone}
+        imgHeight={'133px'}
+        position={'fixed' as 'fixed'}
+      />
+      <BackgroundImage
+        left={'3%'}
+        bottom={'6%'}
+        src={Bulby}
+        imgHeight={'250px'}
+        position={'fixed' as 'fixed'}
+      />
+    </MediaQuery>
   </MaxWidthBox>
 );
 
