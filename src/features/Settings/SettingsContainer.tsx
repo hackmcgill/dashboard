d1b57a4cb965a9610ca290c7f06faf490a4ec2d6
Pierre Theo Klein <pierre.klein@mail.mcgill.ca>
2020-08-01 23:41:35 -0400

    Wire in SettingsForm into SettingsContainer

--- src/features/Settings/SettingsContainer.tsx
+++ src/features/Settings/SettingsContainer.tsx
@@ -1,18 +1,20 @@
 import { Flex } from '@rebass/grid';
 import React, { useEffect, useState } from 'react';
 import Helmet from 'react-helmet';
+import { toast } from 'react-toastify';
 import { Settings } from '../../api/settings';
 import { HACKATHON_NAME, ISetting } from '../../config';
 import { H1, MaxWidthBox } from '../../shared/Elements';
 import ValidationErrorGenerator from '../../shared/Form/validationErrorGenerator';
 import WithToaster from '../../shared/HOC/withToaster';
 import theme from '../../shared/Styles/theme';
+import SettingsForm from './SettingsForm';
 
 const SettingsContainer: React.FC = () => {
   const [settings, setSettings] = useState<ISetting>({
-    openTime: '',
-    closeTime: '',
-    confirmTime: '',
+    openTime: new Date().toISOString(),
+    closeTime: new Date().toISOString(),
+    confirmTime: new Date().toISOString(),
   });
   useEffect(() => {
     getSettings(setSettings);
@@ -31,16 +33,27 @@ const SettingsContainer: React.FC = () => {
         >
           {HACKATHON_NAME} Settings
         </H1>
-        {settings.openTime},{settings.closeTime},{settings.confirmTime},
+        <SettingsForm settings={settings} submit={patchSettings} />
       </MaxWidthBox>
     </Flex>
   );
 };
 
-async function getSettings(setSettings: (settings: ISetting) => void) {
+async function getSettings(callback: (settings: ISetting) => void) {
   try {
     const settings: ISetting = (await Settings.get()).data.data;
-    setSettings(settings);
+    callback(settings);
+  } catch (e) {
+    if (e && e.data) {
+      ValidationErrorGenerator(e.data);
+    }
+  }
+}
+
+async function patchSettings(newSettings: ISetting) {
+  try {
+    await Settings.update(newSettings);
+    toast.success('Settings updated');
   } catch (e) {
     if (e && e.data) {
       ValidationErrorGenerator(e.data);
