8e2f599af4c03859be99c1551073e6aa1f6ab214
Yun Kai Peng <yunkaipeng0129@gmail.com>
2020-08-20 18:20:50 -0400

    refactor: SponsorManagement to SponsorProfileForm

---
+++ src/features/Sponsor/SponsorProfileForm.tsx
@@ -0,0 +1,205 @@
+import React, { useState, useEffect } from 'react';
+import { Helmet } from 'react-helmet';
+import { Redirect, RouteProps } from 'react-router';
+
+import {
+  ErrorMessage,
+  FastField,
+  Formik,
+  FormikProps,
+  FormikValues,
+} from 'formik';
+
+import { Account, Sponsor } from '../../api';
+import {
+  FrontendRoute,
+  HACKATHON_NAME,
+  ISponsor,
+  UserType,
+} from '../../config';
+import * as CONSTANTS from '../../config/constants';
+import { FormDescription, H1, MaxWidthBox } from '../../shared/Elements';
+import { Form, SubmitBtn } from '../../shared/Form';
+import * as FormikElements from '../../shared/Form/FormikElements';
+import ValidationErrorGenerator from '../../shared/Form/validationErrorGenerator';
+import WithToasterContainer from '../../shared/HOC/withToaster';
+
+import getValidationSchema from './validationSchema';
+
+export enum ManageSponsorModes {
+  CREATE,
+  EDIT,
+}
+
+interface ISponsorProfileFormValues extends FormikValues {
+  company: string;
+  url: string;
+}
+
+interface IManageSponsorContainerProps extends RouteProps {
+  mode: ManageSponsorModes;
+}
+
+const ManageSponsorContainer: React.FC<IManageSponsorContainerProps> = (
+  props
+) => {
+  const [formSubmitted, setFormSubmitted] = useState<boolean>(false);
+  const [mode, setMode] = useState<ManageSponsorModes>(props.mode);
+  const [sponsorDetails, setSponsorDetails] = useState<ISponsor>({
+    id: '',
+    tier: 0,
+    accountId: '',
+    company: '',
+    contractURL: 'https://default_url.com',
+    nominees: [],
+  });
+
+  useEffect(() => {
+    (async () => {
+      if (mode === ManageSponsorModes.EDIT) {
+        try {
+          const response = await Sponsor.getSelf();
+          const spDetails = response.data.data;
+          setSponsorDetails(spDetails);
+        } catch (e) {
+          if (e && e.data) {
+            ValidationErrorGenerator(e.data);
+          }
+          // For some reason we could not get self. We should switch our state to CREATE.
+          setMode(ManageSponsorModes.CREATE);
+        }
+      }
+    })();
+  }, []);
+
+  const renderFormik = (fp: FormikProps<any>) => {
+    return (
+      <Form onSubmit={fp.handleSubmit}>
+        <FastField
+          id="companyName"
+          name={'company'}
+          component={FormikElements.Input}
+          label={CONSTANTS.SPONSOR_COMPANY_LABEL}
+          required={true}
+        />
+        <ErrorMessage component={FormikElements.Error} name="company" />
+
+        <SubmitBtn isLoading={fp.isSubmitting} disabled={fp.isSubmitting}>
+          Submit
+        </SubmitBtn>
+      </Form>
+    );
+  };
+
+  const handleSubmit = async (values: ISponsorProfileFormValues) => {
+    try {
+      await submit(values);
+      setFormSubmitted(true);
+    } catch (e) {
+      if (e && e.data) {
+        ValidationErrorGenerator(e.data);
+      }
+    }
+  };
+
+  const submit = async (values: ISponsorProfileFormValues): Promise<void> => {
+    const acctResponse = await Account.getSelf();
+
+    const account = acctResponse.data.data;
+    let sponsorTier = 0;
+
+    switch (account.accountType) {
+      case UserType.SPONSOR_T1:
+        sponsorTier = 1;
+        break;
+      case UserType.SPONSOR_T2:
+        sponsorTier = 2;
+        break;
+      case UserType.SPONSOR_T3:
+        sponsorTier = 3;
+        break;
+      case UserType.SPONSOR_T4:
+        sponsorTier = 4;
+        break;
+      case UserType.SPONSOR_T5:
+        sponsorTier = 5;
+        break;
+      default:
+        sponsorTier = -1;
+    }
+
+    const sponsorApplication = convertFormikToSponsor(
+      values,
+      sponsorDetails.id,
+      account.id,
+      sponsorTier,
+      sponsorDetails.contractURL
+    );
+
+    switch (mode) {
+      case ManageSponsorModes.CREATE:
+        await Sponsor.create(sponsorApplication);
+        break;
+      case ManageSponsorModes.EDIT:
+        await Sponsor.update(sponsorApplication);
+        break;
+    }
+  };
+
+  const convertFormikToSponsor = (
+    values: FormikValues,
+    sponsorId: string,
+    accountId: string,
+    sponsorTier: number,
+    contractURL: string
+  ): ISponsor => {
+    return {
+      id: sponsorId,
+      accountId,
+      tier: sponsorTier,
+      company: values.company,
+      contractURL,
+      nominees: values.nominees,
+    };
+  };
+
+  if (formSubmitted) {
+    return <Redirect to={FrontendRoute.HOME_PAGE} />;
+  }
+
+  return (
+    <MaxWidthBox m={'auto'} maxWidth={'500px'}>
+      <Helmet>
+        <title>
+          {mode === ManageSponsorModes.CREATE ? 'Create' : 'Edit'} Sponsor
+          Profile | {HACKATHON_NAME}
+        </title>
+      </Helmet>
+      <MaxWidthBox maxWidth={'500px'} m={'auto'}>
+        <H1
+          color={'#F2463A'}
+          fontSize={'30px'}
+          textAlign={'left'}
+          marginTop={'0px'}
+          marginBottom={'20px'}
+          marginLeft={'0px'}
+        >
+          {mode === ManageSponsorModes.CREATE ? 'Create' : 'Edit'} your Sponsor
+          profile
+        </H1>
+        <FormDescription>{CONSTANTS.REQUIRED_DESCRIPTION}</FormDescription>
+      </MaxWidthBox>
+      <Formik
+        enableReinitialize={true}
+        initialValues={{
+          company: sponsorDetails.company,
+        }}
+        onSubmit={handleSubmit}
+        render={renderFormik}
+        validationSchema={getValidationSchema()}
+      />
+    </MaxWidthBox>
+  );
+};
+
+export default WithToasterContainer(ManageSponsorContainer);
--- src/pages/Sponsor/Edit.tsx
+++ src/pages/Sponsor/Edit.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { RouteProps } from 'react-router';
-import ManageSponsor, { ManageSponsorModes } from '../../features/Sponsor/SponsorManagement';
+import ManageSponsor, {
+  ManageSponsorModes,
+} from '../../features/Sponsor/SponsorProfileForm';
 
 const EditSponsorPage: React.FC = (props: RouteProps) => {
   return <ManageSponsor mode={ManageSponsorModes.EDIT} {...props} />;
