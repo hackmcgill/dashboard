d4a39d2cfeb316e4f3e213dca56fb3617a364d22
Pierre Theo Klein <pierre.klein@mail.mcgill.ca>
2020-10-14 22:31:02 -0400

    refactor: break up the Travel page into several subcomponents, and move const strings into CONSTANTS file (#866)
    * refactor: break up the Travel page into several subcomponents, and move const strings into CONSTANTS file.

---
+++ src/pages/Travel/TravelStatusBus.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+
+import {
+  BUS_DEPARTURE_CITY,
+  BUS_SHOPIFY_PAGE,
+  BUS_SLACK_CHANNEL,
+  BUS_SLACK_PAGE,
+  HACKATHON_NAME,
+  ITravel,
+} from '../../config';
+import { LinkDuo } from '../../shared/Elements';
+
+interface ITravelStatusProps {
+  travel: ITravel;
+}
+
+const TravelStatusBus: React.FunctionComponent<ITravelStatusProps> = ({
+  travel,
+}) => {
+  return (
+    <div>
+      Congratulations, you've secured a seat on our {BUS_DEPARTURE_CITY} bus{' '}
+      to/from {HACKATHON_NAME}!
+      <br />
+      <br />
+      <h2>Bus</h2>
+      Join the {BUS_SLACK_CHANNEL} on our official{' '}
+      <LinkDuo to={BUS_SLACK_PAGE}>Slack</LinkDuo> for details and more
+      information about your bus route.
+      <br />
+      <br />
+      If you can no longer make it to McHacks, please{' '}
+      <LinkDuo to={BUS_SHOPIFY_PAGE}>contact</LinkDuo>
+      us so we can refund your deposit and open the seat up to another hacker.
+    </div>
+  );
+};
+
+export default TravelStatusBus;
