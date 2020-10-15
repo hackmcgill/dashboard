89b4d4e118fef2a3439ae586ff7ff8e5ae16630e
James Xu <36768789+jamesxu123@users.noreply.github.com>
2020-10-14 20:06:41 -0400

    Feature: Application Pagination (#870)
    * WIP: Create application Pagination component
    
    * Implement new state logic and add lastCompleted prop for PaginationHeaderComponent.tsx
    
    * Remove state from PaginationHeaderComponent.tsx and embed component into ManageApplicationContainer.tsx
    
    * Fix: made bar between bubble's width dynamic
    
    * Fix: dynamic bubble width
    
    * Revert back to 160px width
    
    * fix: remove hard-coded colors and un-used state
    
    * Refactor: use function components instead
    
    * Remove unused import
    
    * Remove commented-out code
    
    * Refactor PaginationHeaderComponent
    * move TOTAL_PAGES into /src/config/constants
    * improve code consistency
    * separate PaginationHeaderComponent.tsx sub-components to individual files
    
    * Commenting run through
    
    * Improve comment consistency

---
+++ src/features/Application/PaginationHeader/NumberPageText.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import done from '../../../assets/images/done.svg';
+import { Image, Paragraph } from '../../../shared/Elements';
+import theme from '../../../shared/Styles/theme';
+
+interface INumberPageText {
+  pageNumber: number;
+  fill: boolean;
+  check: boolean;
+}
+
+/**
+ * Displays either a checkmark or page number depending on props
+ * Should be children prop of NumberBubble
+ */
+const NumberPageText: React.FC<INumberPageText> = (props) => {
+  if (!props.check) {
+    // Number only displays if we are not displaying a checkmark
+    const notSelectedTextStyle = {
+      position: 'relative' as 'relative',
+      fontSize: '12px',
+      color: theme.colors.black40,
+      textAlign: 'center' as 'center',
+    };
+
+    const selectedTextStyle = {
+      ...notSelectedTextStyle,
+      color: 'white',
+    };
+    const textStyle = props.fill ? selectedTextStyle : notSelectedTextStyle; // Style changes depending on props.fill
+
+    return <Paragraph style={textStyle}>{props.pageNumber}</Paragraph>;
+  } else {
+    // Checkmark svg
+    return <Image src={done} />;
+  }
+};
+
+export default NumberPageText;
