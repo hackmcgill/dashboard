280cb68bbc64646f860a77c8a779f43ecb753a46
James Xu <xujames007@gmail.com>
2020-10-24 14:37:40 -0400

    style: removed unused comments

--- src/shared/Elements/GridTwoColumn.tsx
+++ src/shared/Elements/GridTwoColumn.tsx
@@ -1,16 +1,4 @@
-// import React from 'react';
 import styled from 'styled-components';
-
-// const GridTwoColumn: React.FC = (props) => {
-//   const style = {
-//     display: 'grid',
-//     gridTemplateColumns: 'repeat( auto-fill, minmax(min(80vw, 400px), 1fr) )',
-//     columnGap: '80px',
-//     rowGap: '20px',
-//     marginBottom: '60px',
-//   };
-//   return <div style={style}>{props.children}</div>;
-// };
 
 export interface IGridTwoColumnProps {
   columnWidth?: string;
