182420181ff347237ed2425138c494c6c84ec666
logan-r <logan.ralston@mail.mcgill.ca>
2020-11-12 21:17:25 -0500

    New inteface for create/join team

--- src/features/Team/JoinCreateTeam.tsx
+++ src/features/Team/JoinCreateTeam.tsx
@@ -1,14 +1,5 @@
 import React, { useState } from 'react';
 
-import { Box, Flex } from '@rebass/grid';
-import {
-  ErrorMessage,
-  FastField,
-  Formik,
-  FormikProps,
-  FormikValues,
-} from 'formik';
-
 import {
   Button,
   ButtonVariant,
@@ -16,14 +7,12 @@ import {
 
 import { IHacker } from '../../config';
 
-import Team from '../../api/team';
-import { Form } from '../../shared/Form';
+// import Team from '../../api/team';
+import { Form, Label } from '../../shared/Form';
 import {
-  Error as ErrorComponent,
   Input,
-} from '../../shared/Form/FormikElements';
-import ValidationErrorGenerator from '../../shared/Form/validationErrorGenerator';
-import getValidationSchema from './validationSchema';
+} from '../../shared/Form';
+import theme from '../../shared/Styles/theme';
 
 interface IJoinCreateTeamProps {
   hacker: IHacker;
@@ -32,99 +21,87 @@ interface IJoinCreateTeamProps {
 
 const JoinCreateTeam: React.FC<IJoinCreateTeamProps> = (props) => {
   // Is the user currently trying to join or create a team?
-  const [isLoading, setIsLoading] = useState(false);
+  const isLoading = useState(false)[0];
 
   // Keep track of which button was clicked
-  const [submissionBtn, setSubmissionBtn] = useState(0);
+  // const [submissionBtn, setSubmissionBtn] = useState(0);
 
-  const renderFormik = (fp: FormikProps<any>) => {
-    return (
-      <Form>
-        <FastField
-          name={'name'}
-          component={Input}
-          label={'Enter your team name:'}
-          placeholder={'Team name'}
-          value={fp.values.name}
-          required={true}
-        />
-        <ErrorMessage component={ErrorComponent} name="name" />
-        <Flex justifyContent={'flex-end'}>
-          <Box mr={'12px'}>
-            <Button
-              type="button"
-              variant={ButtonVariant.Secondary}
-              onClick={onClickFactory(0, fp.submitForm)}
-              isLoading={isLoading}
-              disabled={isLoading}
-            >
-              Create new team
-            </Button>
-          </Box>
-          <Box>
-            <Button
-              type="button"
-              variant={ButtonVariant.Secondary}
-              onClick={onClickFactory(1, fp.submitForm)}
-              isLoading={isLoading}
-              disabled={isLoading}
-            >
-              Join team
-            </Button>
-          </Box>
-        </Flex>
-      </Form>
-    );
-  }
-
-  const onClickFactory = (
-    btn: number,
-    submitForm: () => void
-  ): (e: any) => void => {
-    return (e) => {
-      setSubmissionBtn(btn)
-      submitForm();
-    };
-  }
-
-  const handleSubmit = async (values: FormikValues) => {
-    setIsLoading(true);
-    if (submissionBtn === 0) {
-      try {
-        await Team.create({
-          name: values.name,
-          members: [props.hacker.id],
-        });
-        props.onTeamChange();
-      } catch (e) {
-        if (e.status === 409) {
-          if (e && e.data) {
-            ValidationErrorGenerator(e.data);
-          }
-        }
-      }
-    } else {
-      try {
-        await Team.join(values.name);
-        props.onTeamChange();
-      } catch (e) {
-        if (e && e.data) {
-          ValidationErrorGenerator(e.data);
-        }
-      }
-    }
-    setIsLoading(false);
-  }
+  // const handleSubmit = async (values: FormikValues) => {
+  //   setIsLoading(true);
+  //   if (submissionBtn === 0) {
+  //     try {
+  //       await Team.create({
+  //         name: values.name,
+  //         members: [props.hacker.id],
+  //       });
+  //       props.onTeamChange();
+  //     } catch (e) {
+  //       if (e.status === 409) {
+  //         if (e && e.data) {
+  //           ValidationErrorGenerator(e.data);
+  //         }
+  //       }
+  //     }
+  //   } else {
+  //     try {
+  //       await Team.join(values.name);
+  //       props.onTeamChange();
+  //     } catch (e) {
+  //       if (e && e.data) {
+  //         ValidationErrorGenerator(e.data);
+  //       }
+  //     }
+  //   }
+  //   setIsLoading(false);
+  // }
 
   return (
-    <Formik
-      initialValues={{
-        name: '',
-      }}
-      onSubmit={handleSubmit}
-      render={renderFormik}
-      validationSchema={getValidationSchema}
-    />
+    <>
+      <div className="create">
+        <Button
+          variant={ButtonVariant.Primary}
+          isLoading={isLoading}
+          disabled={isLoading}
+        >
+          Create new team
+        </Button>
+      </div>
+
+      <Form>
+        <div className="join">
+          <Label style={{ maxWidth: '272px' }}>
+            Already have a team?
+            <Input type="text" name="name" placeholder="Enter team code" style={{ marginBottom: 0 }} />
+          </Label>
+
+          <Button
+            type="button"
+            variant={ButtonVariant.Secondary}
+            isLoading={isLoading}
+            disabled={isLoading}
+          >
+            Join team
+          </Button>
+        </div>
+      </Form>
+
+      <style jsx>{`
+        .create {
+          text-align: center;
+
+          padding-top: 8px;
+          padding-bottom: 48px;
+          border-bottom: 1px solid ${theme.colors.purpleLight};
+          margin-bottom: 32px;
+        }
+
+        .join {
+          display: flex;
+          justify-content: space-between;
+          align-items: flex-end;
+        }
+      `}</style>
+    </>
   );
 }
 
