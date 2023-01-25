e7e3f5a695a91577dbb9378ea9924cedd96051f0
Anmol Brar <83686967+brarsanmol@users.noreply.github.com>
2023-01-25 16:30:55 -0500

    chore: develop to v3
    * feat: remove remote options on acceptance
    
    * fix: remove attendance pref from review
    
    * fix(GeneralOnboarding.tsx): update onboarding information for mchacks 9.
    
    - update sponsor dashboard for McHacks9
    
    - update GeneralOnboarding.tsx
    
    * chore: release 2.5.3
    
    * fix: update discord onboarding guide to mchacks 9.
    
    * feat(GeneralOnboarding.tsx): add sponsor specific schedule to onboarding page.
    
    * feat: remove attendance preference
    
    * fix: remove unused type
    
    * feat: remove attendance preference
    
    * fix: remove unused type
    
    * fix: remove attendance pref from review
    
    * feat: remove attendance preference
    
    * fix: remove unused type
    
    * fix: remove attendance pref from review
    
    Co-authored-by: jenniferchen11 <jennifer.chen2002@gmail.com>
    
    * Revert "feat: Add popup before hacker confirmation to collect location info (#943)"
    
    This reverts commit 2aef40fc85c8435c77ddeaa55c621deb398c48c4.
    
    Co-authored-by: jenniferchen11 <jennifer.chen2002@gmail.com>
    
    * feat: remove travel page
    
    * fix: remove unused imports on App.tsx
    
    * feat: travel update (remove busses) (#977)
    
    * feat: travel update (remove busses)
    
    * fix: remove bus messages and unused imports
    
    * feat: updated sponsor dashboard
    
    * feat: updated sponsor dashboard
    
    * fix: further updates to timing and removing discord specific wording
    
    * fix: removed live site and updated discord and schedule links

--- src/features/Onboarding/GeneralOnboarding.tsx
+++ src/features/Onboarding/GeneralOnboarding.tsx
@@ -12,8 +12,8 @@ const GeneralOnboarding: React.FC = () => (
           <h2>Introduction</h2>
           <p>
             Hey there, sponsor! We’re looking forward to seeing you for round
-            nine of McHacks, Canada’s original collegiate hackathon, on{' '}
-            <b>January 21th! </b>
+            ten of McHacks, Canada’s original collegiate hackathon, on{' '}
+            <b>January 28th! </b>
             Before the event, we’d like to offer you a pre-event outline of what
             to expect on the day-of.
             <br />
@@ -24,7 +24,7 @@ const GeneralOnboarding: React.FC = () => (
             <br />
             <br />
             For a sponsor specific schedule, you can find it{' '}
-            <LinkDuo to="https://docs.google.com/spreadsheets/d/1p1RxMcBnCGBlBJMznkvuW9u_rYtbbKno15VjXfrolao/edit?usp=sharing">
+            <LinkDuo to="https://docs.google.com/document/d/1ClrMBzOffam8vU00YCq_YPFAVaYFnBKR0CEILWQsirg/edit?usp=sharing">
               here
             </LinkDuo>
             . We will be updating it as we continue receiving information from
@@ -36,47 +36,47 @@ const GeneralOnboarding: React.FC = () => (
             Discord. we recommend that you read our Discord Onboarding Guide
             first if you don’t know what Discord is. Once you are ready, join
             the channel by clicking
-            <LinkDuo to="https://discord.gg/fqbjr43cKf">
+            <LinkDuo to="https://discord.com/invite/6W25XWfHUr">
               <span> this link. </span>
             </LinkDuo>
             <br />
             <br />
-            When you join our Discord, please send a message to Lucy Chen on
+            When you join our Discord, please send a message to Andrew Kan on
             Discord with your name, company, and role so we can add you to the
             channel we will communicate with you on.
             <br />
             <br />
-            On January 21st, sponsor check-in will start at <b>6:00 p.m. </b>on
-            Discord. Once you have logged onto Discord, message your coordinator
+            On January 28th, sponsor check-in will start at <b>8:00 a.m. </b>.
+            Once you have logged onto Discord, message your coordinator
             to let them know you are ready to go! We ask you to check-in at
-            least 15 minutes before opening ceremonies at at <b>7:00 p.m. </b>
+            least 15 minutes before opening ceremonies at at <b>10:00 a.m. </b>
+            We also ask you to fill up the sponsor{' '}
+            <LinkDuo to="https://forms.gle/mdsimQyWJG83vYDZ8">
+              <span>check-in form</span>
+            </LinkDuo>.
             <br />
             <br />
-            If part of your package involves speaking at the opening ceremonies,
-            please ensure your video is uploaded to{' '}
-            <LinkDuo to="https://docs.google.com/forms/d/e/1FAIpQLSf32gYN0w8y7aOFmkWu8krKFNJ6xqv6cT3GRXYk-h3PFsYghg/viewform">
-              <span>this form</span>
-            </LinkDuo>
-            . If you are unable to provide a video, please let your coordinator
+            If you are scheduled to speak during the opening ceremony,
+            please email <b>sponsor@mchacks.ca</b> with your slides or video. If you are unable to provide a video, please let your coordinator
             know for accommodations.
           </p>
           <h2>Sponsors</h2>
           <p>
-            This year, our sponsors with recruiter passes will have their own
-            Discord channels. This is where you can interact with hackers and
+            This year, our sponsors with recruiter passes will have their own tables.
+            This is where you can interact with hackers and
             showcase your products! Make sure you have access to social media so
             that you can share your weekend with our audience and yours! There
-            are two important times we ask you to be online:
+            are two important times we ask you to be present:
             <br />
             <br />
-            <b>1. Tabling, Saturday 10:00am-12:00pm:</b> Only applicable to
+            <b>1. Tabling, Saturday 12:00pm-5:00pm:</b> Only applicable to
             sponsors with tabling in their package. This is your designated
-            sponsor booth slot. All hackers will know that you will be online
-            and in your channels at this time, so they will hop in to chat with
+            sponsor booth slot. All hackers will know that you will be in-person
+            and available at this time, so they will drop by to chat with
             you!
             <br />
             <br />
-            <b>2. Speed Networking, Saturday 2:00pm-4:00pm:</b> Only applicable
+            <b>2. Speed Networking, Saturday 5:00pm-7:00pm:</b> Only applicable
             to sponsors with speed networking in their package. If you would
             like to participate and you haven’t let your coordinator know,
             please do so as soon as possible.
@@ -95,7 +95,7 @@ const GeneralOnboarding: React.FC = () => (
             space where hackers can go themselves and find mentors.
             <br />
             <br />
-            Here is an important time we ask mentors to be online:
+            Here is an important time we ask mentors to be available:
             <br />
             <b>Saturday, 10:00am-12:00pm.</b>
             <br />
@@ -127,7 +127,7 @@ const GeneralOnboarding: React.FC = () => (
             <br />
             If you are interested in judging the other prizes and haven’t told
             your coordinator, please do so as we have a judging workshop on
-            Saturday at <b>4:00p.m.</b> that is mandatory.
+            Sunday at <b>9:00a.m.</b> or <b>10:30a.m.</b> that is mandatory.
           </p>
           <h2>Workshops {'&'} Mini Events</h2>
           If you are hosting a workshop or mini-event, they will be held
@@ -151,8 +151,8 @@ const GeneralOnboarding: React.FC = () => (
             {'{sponsor}'} channel.
             <br />
             <br />
-            In the event of an emergency, <b>contact</b> the co-directors Agnes
-            (+1 438-969-0186) or Logan (+1 908-328-9155). Please also contact
+            In the event of an emergency, <b>contact</b> the co-directors Jenny
+            (+1 905-621-6698) or Elisa (+1 705-790-8412). Please also contact
             any organizer for immediate assistance.
           </p>
         </Text>
