15aece953d6ead4098d13d268604c8c624b90376
allennatang <allennatang@gmail.com>
2024-11-14 18:51:07 -0500

    Added country of residence field to the hacker application.

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -104,6 +104,8 @@ export const COC_MLH_REQUEST_LABEL = 'MLH terms, conditions and guidelines.';
 export const COMMENTS_LABEL = "Is there anything you'd like us to know?";
 export const COUNTRY_LABEL = 'Country of Residence';
 export const COUNTRY_PLACEHOLDER = 'Country of Residence';
+export const COUNTRY_TEXT =
+  'Please indicate your current country of residence.';
 export const DEGREE_LABEL = 'Degree';
 export const DEGREE_PLACEHOLDER = 'High School, Undergraduate...';
 export const DRIBBBLE_LINK_LABEL = 'Dribbble URL';
