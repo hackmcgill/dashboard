3b34300956f834b4e36742c285fd5a8c0a382027
Tavi Pollard <45189395+tektaxi@users.noreply.github.com>
2024-11-14 21:17:58 -0500

    MLH Form Changes (#1016)
    * Updated level of study options to match MLH requirements
    
    * #1000 - Update export from .tsv to .csv
    
    * Changed hacker account form 'birthdate' field to 'age' #1003
    
    * Added country of residence field to Other/Personal Details section of hacker application #1009
    
    * Added country of residence field to the hacker application.
    
    * add back CEGEP level of study.
    
    ---------
    
    Co-authored-by: allennatang <allennatang@gmail.com>

--- src/features/Application/ManageApplicationForm.tsx
+++ src/features/Application/ManageApplicationForm.tsx
@@ -20,6 +20,7 @@ import PaginationHeader from './PaginationHeader/PaginationHeader';
 import getValidationSchema from './validationSchema';
 
 import {
+  Countries,
   Degrees,
   FrontendRoute,
   HackerStatus,
@@ -110,6 +111,7 @@ const ManageApplicationForm: React.FunctionComponent<
       },
       other: {
         ethnicity: [],
+        country: '',
         privacyPolicy: false,
         codeOfConduct: false,
       },
@@ -463,6 +465,22 @@ const ManageApplicationForm: React.FunctionComponent<
               <ErrorMessage
                 component={FormikElements.Error}
                 name="hacker.application.other.ethnicity"
+              />
+
+              <FastField
+                name={'hacker.application.other.country'}
+                options={Countries}
+                isMulti={false}
+                creatable={false}
+                component={FormikElements.Select}
+                label={CONSTANTS.COUNTRY_LABEL}
+                placeholder={CONSTANTS.COUNTRY_PLACEHOLDER}
+                value={fp.values.hacker.application.other.country}
+                required={true}
+              />
+              <ErrorMessage
+                component={FormikElements.Error}
+                name="hacker.application.other.country"
               />
             </div>
           </div>
@@ -1095,6 +1113,12 @@ const ManageApplicationForm: React.FunctionComponent<
               <div className="name">{CONSTANTS.ETHNICITY_LABEL}</div>
               <div className="value">
                 {hackerDetails.application.other.ethnicity}
+              </div>
+            </div>
+            <div className="field">
+              <div className="name">{CONSTANTS.COUNTRY_LABEL}</div>
+              <div className="value">
+                {hackerDetails.application.other.country}
               </div>
             </div>
           </GridTwoColumn>
