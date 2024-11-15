0394c3ea467ddd419d4a4a7ef5a9cce79c246657
allennatang <allennatang@gmail.com>
2024-11-14 19:44:33 -0500

    Updated level of study options to match MLH requirements

--- src/config/degrees.ts
+++ src/config/degrees.ts
@@ -1,7 +1,14 @@
 export enum Degrees {
-  HIGHSCHOOL = 'High School',
-  CEGEP = 'CEGEP',
-  UNDERGRADUATE = 'Undergraduate',
-  GRADUATE = 'Graduate',
+  LESS_THAN_SECONDARY = 'Less than Secondary / High School',
+  SECONDARY = 'Secondary / High School',
+  COLLEGE = 'Undergraduate University (2 year - community college or similar)',
+  UNDERGRADUATE = 'Undergraduate University (3+ year)',
+  GRADUATE = 'Graduate University (Masters, Professional, Doctoral, etc)',
+  CODE_SCHOOL = 'Code School / Bootcamp',
+  VOCATIONAL = 'Other Vocational / Trade Program or Apprenticeship',
+  POSTDOC = 'Post Doctorate',
+  OTHER = 'Other',
+  NOT_STUDENT = "I'm not currently a student",
+  NO_ANSWER = 'Prefer not to answer'
 }
 export default Degrees;
