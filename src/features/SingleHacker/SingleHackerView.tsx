f762d54536aac928ca5d080a033a24258c4fc58f
JAMIE XIAO <jamie.xiao.ca@gmail.com>
2025-04-04 12:41:41 -0400

    Cleaner code

--- src/features/SingleHacker/SingleHackerView.tsx
+++ src/features/SingleHacker/SingleHackerView.tsx
@@ -50,11 +50,6 @@ const SingleHackerView: React.FC<IHackerViewProps> = (props) => {
   const [isAdmin, setIsAdmin] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
 
-  // console.log("reviewerName:", props.hacker.reviewerName);
-  console.log("reviewerComments:", props.hacker.reviewerComments);
-  console.log("props.hacker:", props.hacker);
-  console.log("reviewerComments State: ", reviewerComments)
-
   useEffect(() => {
     setIsAdmin(props.userType === UserType.STAFF);
   }, [props.userType]);
@@ -84,20 +79,6 @@ const SingleHackerView: React.FC<IHackerViewProps> = (props) => {
     }
   };
 
-  // const submitReviewer = async () => {
-  //   try {
-  //     const { hacker } = props;
-  //     setIsLoading(true);
-  //     await Hacker.updateReviewerStatus(hacker.id, reviewerStatus);
-  //     setIsLoading(false);
-  //     toast.success(`Hacker status updated to ${reviewerStatus}!`);
-  //   } catch (e: any) {
-  //     if (e && e.data) {
-  //       ValidationErrorGenerator(e.data);
-  //     }
-  //   }
-  // };
-
   const handleChange = ({ value }: any) => {
     setStatus(value);
   };
@@ -116,9 +97,7 @@ const SingleHackerView: React.FC<IHackerViewProps> = (props) => {
   
   const handleReviewerNameChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
     try {
-      setReviewerName(event.target.value);
-      console.log(event.target.value);
-      console.log(reviewerName);     
+      setReviewerName(event.target.value);   
       await submitReviewerName();
     } catch (error) {
       console.error('Error updating reviewer status:', error);
@@ -128,10 +107,7 @@ const SingleHackerView: React.FC<IHackerViewProps> = (props) => {
   const submitReviewerName = async () => {
     try {
       const { hacker } = props;
-      // const updatedReviewerStatus = reviewerStatusOverride || reviewerStatus;
-      // console.log('REVIEW', reviewerStatus);
       await Hacker.updateReviewerName(hacker.id, reviewerName);
-      console.log("name: ", props.hacker.reviewerName);
       // toast.success(`Hacker status updated to ${reviewerStatus}!`);
     } catch (e: any) {
       if (e?.data) {
@@ -145,8 +121,6 @@ const SingleHackerView: React.FC<IHackerViewProps> = (props) => {
   const handleReviewerNameChange2 = async (event: React.ChangeEvent<HTMLInputElement>) => {
     try {
       setReviewerName2(event.target.value);
-      console.log(event.target.value);
-      console.log(reviewerName2);     
       await submitReviewerName2();
     } catch (error) {
       console.error('Error updating reviewer status:', error);
@@ -156,10 +130,7 @@ const SingleHackerView: React.FC<IHackerViewProps> = (props) => {
   const submitReviewerName2 = async () => {
     try {
       const { hacker } = props;
-      // const updatedReviewerStatus = reviewerStatusOverride || reviewerStatus;
-      // console.log('REVIEW', reviewerStatus);
       await Hacker.updateReviewerName2(hacker.id, reviewerName2);
-      console.log("name: ", props.hacker.reviewerName2);
       // toast.success(`Hacker status updated to ${reviewerStatus}!`);
     } catch (e: any) {
       if (e?.data) {
@@ -185,8 +156,6 @@ const SingleHackerView: React.FC<IHackerViewProps> = (props) => {
   const handleReviewerCommentsChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
     try {
       setReviewerComments(event.target.value);
-      console.log("value: ", event.target.value);
-      console.log("element: ",reviewerComments);     
       await submitReviewerComments();
     } catch (error) {
       console.error('Error updating reviewer status:', error);
@@ -196,10 +165,7 @@ const SingleHackerView: React.FC<IHackerViewProps> = (props) => {
   const submitReviewerComments = async () => {
     try {
       const { hacker } = props;
-      // const updatedReviewerStatus = reviewerStatusOverride || reviewerStatus;
-      // console.log('REVIEW', reviewerStatus);
       await Hacker.updateReviewerComments(hacker.id, reviewerComments);
-      console.log("name: ", props.hacker.reviewerComments);
       // toast.success(`Hacker status updated to ${reviewerStatus}!`);
     } catch (e: any) {
       if (e?.data) {
@@ -212,9 +178,7 @@ const SingleHackerView: React.FC<IHackerViewProps> = (props) => {
   
   const handleReviewerCommentsChange2 = async (event: React.ChangeEvent<HTMLInputElement>) => {
     try {
-      setReviewerComments2(event.target.value);
-      console.log(event.target.value);
-      console.log(reviewerComments2);     
+      setReviewerComments2(event.target.value);    
       await submitReviewerComments2();
     } catch (error) {
       console.error('Error updating reviewer status:', error);
@@ -224,10 +188,7 @@ const SingleHackerView: React.FC<IHackerViewProps> = (props) => {
   const submitReviewerComments2 = async () => {
     try {
       const { hacker } = props;
-      // const updatedReviewerStatus = reviewerStatusOverride || reviewerStatus;
-      // console.log('REVIEW', reviewerStatus);
       await Hacker.updateReviewerComments2(hacker.id, reviewerComments2);
-      console.log("name: ", props.hacker.reviewerComments2);
       // toast.success(`Hacker status updated to ${reviewerStatus}!`);
     } catch (e: any) {
       if (e?.data) {
@@ -250,60 +211,6 @@ const SingleHackerView: React.FC<IHackerViewProps> = (props) => {
     }
   }, [reviewerStatus2]);
 
-  // const handleReviewerChange = async ({ value }: any, reviewerNum: number) => {
-  //   if (reviewerNum == 1) {
-  //     try {      
-  //       setReviewerStatus(value);      
-  //       await submitReviewer(1);
-  //     } catch (error) {
-  //       console.error('Error updating reviewer status:', error);
-  //     }
-  //   }
-  //   else if (reviewerNum == 2) {
-  //     try {      
-  //       setReviewerStatus2(value);      
-  //       await submitReviewer(2);
-  //     } catch (error) {
-  //       console.error('Error updating reviewer status:', error);
-  //     }
-  //   }
-  // };
-
-  // const submitReviewer = async (num: number) => {
-  //   if (num == 1) {
-  //     try {
-  //       const { hacker } = props;
-  //       // const updatedReviewerStatus = reviewerStatusOverride || reviewerStatus;
-  //       // console.log('REVIEW', reviewerStatus);
-  //       await Hacker.updateReviewerStatus(hacker.id, reviewerStatus);
-  //       // console.log(reviewerStatus);
-  //       // toast.success(`Hacker status updated to ${reviewerStatus}!`);
-  //     } catch (e: any) {
-  //       if (e?.data) {
-  //         ValidationErrorGenerator(e.data);
-  //       } else {
-  //         console.error('Unexpected error:', e);
-  //       }
-  //     }
-  //   }
-  //   else if (num == 2) {
-  //     try {
-  //       const { hacker } = props;
-  //       // const updatedReviewerStatus = reviewerStatusOverride || reviewerStatus;
-  //       // console.log('REVIEW', reviewerStatus);
-  //       await Hacker.updateReviewerStatus2(hacker.id, reviewerStatus2);
-  //       // console.log(reviewerStatus);
-  //       // toast.success(`Hacker status updated to ${reviewerStatus}!`);
-  //     } catch (e: any) {
-  //       if (e?.data) {
-  //         ValidationErrorGenerator(e.data);
-  //       } else {
-  //         console.error('Unexpected error:', e);
-  //       }
-  //     }
-  //   }
-  // };
-
   const handleReviewerChange = async ({ value }: any) => {
     try {      
       setReviewerStatus(value);      
@@ -316,10 +223,7 @@ const SingleHackerView: React.FC<IHackerViewProps> = (props) => {
   const submitReviewer = async () => {
     try {
       const { hacker } = props;
-      // const updatedReviewerStatus = reviewerStatusOverride || reviewerStatus;
-      // console.log('REVIEW', reviewerStatus);
       await Hacker.updateReviewerStatus(hacker.id, reviewerStatus);
-      // console.log(reviewerStatus);
       // toast.success(`Hacker status updated to ${reviewerStatus}!`);
     } catch (e: any) {
       if (e?.data) {
@@ -342,10 +246,7 @@ const SingleHackerView: React.FC<IHackerViewProps> = (props) => {
   const submitReviewer2 = async () => {
     try {
       const { hacker } = props;
-      // const updatedReviewerStatus = reviewerStatusOverride || reviewerStatus;
-      // console.log('REVIEW', reviewerStatus);
       await Hacker.updateReviewerStatus2(hacker.id, reviewerStatus2);
-      // console.log(reviewerStatus);
       // toast.success(`Hacker status updated to ${reviewerStatus}!`);
     } catch (e: any) {
       if (e?.data) {
@@ -485,13 +386,6 @@ const SingleHackerView: React.FC<IHackerViewProps> = (props) => {
                     value={reviewerName2}
                   />
                 </Box>
-                {/* <Box width={[1, 1/2]} style={{ paddingTop: '-10px', marginRight: '17px' }}>
-                  <Input
-                    onChange={handleReviewerNameChange2}
-                    placeholder={'Reviewer Name'}
-                    value={reviewerName2}
-                  />
-                </Box> */}
                 <Box width={'9.1rem'} style={{ paddingTop: '-10px' }}>
                   <StyledSelect
                     isTight={true}
@@ -514,8 +408,6 @@ const SingleHackerView: React.FC<IHackerViewProps> = (props) => {
                   ml="0px"
                   mt="-27px"
               >
-
-                {/* <Box width={'35rem'} style={{ paddingTop: '0px', marginRight: '17px' }}> */}
                 <Box width={[1, 0.7]} style={{ paddingTop: '0px', marginRight: '17px' }}>
                   <Input
                     onChange={handleReviewerCommentsChange2}
@@ -647,258 +539,3 @@ const SingleHackerView: React.FC<IHackerViewProps> = (props) => {
 };
 
 export default SingleHackerView;
-
-
-// import React, { useEffect, useState } from 'react';
-// import Helmet from 'react-helmet';
-
-// import { Box, Flex } from '@rebass/grid';
-// import { toast } from 'react-toastify';
-
-// import { Hacker } from '../../api';
-// import {
-//   HACKATHON_NAME,
-//   HackerStatus,
-//   IAccount,
-//   IHacker,
-//   UserType,
-// } from '../../config';
-// import {
-//   Button,
-//   ButtonVariant,
-//   H1,
-//   H2,
-//   MaxWidthBox,
-// } from '../../shared/Elements';
-// import ViewPDFComponent from '../../shared/Elements/ViewPDF';
-// import { Form, StyledSelect } from '../../shared/Form';
-// import ValidationErrorGenerator from '../../shared/Form/validationErrorGenerator';
-// import theme from '../../shared/Styles/theme';
-
-// //date2age is currently unused
-// import { date2age, getOptionsFromEnum } from '../../util';
-
-// import SHField from './SingleHackerField';
-// import SHLink from './SingleHackerLink';
-// import SHParagraph from './SingleHackerParagraph';
-// import SingleHackerSection from './SingleHackerSection';
-
-// interface IHackerViewProps {
-//   hacker: IHacker;
-//   userType: UserType;
-// }
-
-// const SingleHackerView: React.FC<IHackerViewProps> = (props) => {
-//   const [status, setStatus] = useState(props.hacker.status);
-//   const [isAdmin, setIsAdmin] = useState(true);
-//   const [isLoading, setIsLoading] = useState(false);
-
-//   useEffect(() => {
-//     setIsAdmin(props.userType === UserType.STAFF);
-//   }, [props.userType]);
-
-//   useEffect(() => {
-//     setStatus(props.hacker.status);
-//   }, [props]);
-
-//   const submit = async () => {
-//     try {
-//       const { hacker } = props;
-//       setIsLoading(true);
-//       await Hacker.updateStatus(hacker.id, status);
-//       setIsLoading(false);
-//       toast.success(`Hacker status updated to ${status}!`);
-//     } catch (e: any) {
-//       if (e && e.data) {
-//         ValidationErrorGenerator(e.data);
-//       }
-//     }
-//   };
-
-//   const handleChange = ({ value }: any) => {
-//     setStatus(value);
-//   };
-
-//   const { hacker } = props;
-//   const account = (hacker.accountId as IAccount) || {};
-//   const pronoun = account.pronoun ? `(${account.pronoun})` : '';
-
-//   return (
-//     <article>
-//       <Helmet>
-//         <title>
-//           {`${account.firstName} ${account.lastName}`} | {HACKATHON_NAME}
-//         </title>
-//       </Helmet>
-//       <MaxWidthBox maxWidth="800px">
-//         <Flex flexDirection={'column'} style={{ marginTop: '4em' }}>
-//           <H1 marginLeft="0">
-//             {`${account.firstName} ${account.lastName} ${pronoun}`}
-//           </H1>
-//         </Flex>
-//         <hr hidden={isAdmin} />
-//         <Box ml="6px">
-//           <SingleHackerSection
-//             title={'Administrative Information'}
-//             hidden={!isAdmin}
-//           >
-//             <Form>
-//               <Flex
-//                 width="100%"
-//                 flexWrap="wrap"
-//                 justifyContent="start"
-//                 alignItems="center"
-//                 mb="16px"
-//               >
-//                 <Box width={[1, 1 / 2]} style={{ paddingTop: '10px' }}>
-//                   <StyledSelect
-//                     isTight={true}
-//                     className="react-select-container"
-//                     classNamePrefix="react-select"
-//                     options={getOptionsFromEnum(HackerStatus)}
-//                     isDisabled={!isAdmin}
-//                     onChange={handleChange}
-//                     value={{
-//                       label: status,
-//                       value: status,
-//                     }}
-//                   />
-//                 </Box>
-//                 <Flex
-//                   justifyContent={['center', 'flex-start']}
-//                   alignItems="center"
-//                   ml="16px"
-//                 >
-//                   <Button
-//                     type="button"
-//                     onClick={submit}
-//                     variant={ButtonVariant.Primary}
-//                     isLoading={isLoading}
-//                     disabled={isLoading || !isAdmin}
-//                   >
-//                     Change status
-//                   </Button>
-//                 </Flex>
-//               </Flex>
-//             </Form>
-//             <Flex
-//               width="100%"
-//               flexWrap="wrap"
-//               justifyContent="space-between"
-//               alignItems="center"
-//             >
-//               <SHField label="Age" text={account.age} />
-//               <SHField
-//                 label="Shirt Size"
-//                 text={hacker.application.accommodation.shirtSize}
-//               />
-//               {/* Removed as shirt size is no longer a property of account
-//                 <SHField label="Shirt Size" text={account.shirtSize} /> */}
-//               <SHField label="Gender" text={account.gender} />
-//               <SHLink
-//                 label="Phone Number"
-//                 link={`tel:${account.phoneNumber}`}
-//                 linkText={account.phoneNumber}
-//               />
-//               <SHField
-//                 label="Dietary Restrictions"
-//                 text={
-//                   account.dietaryRestrictions &&
-//                   account.dietaryRestrictions.join(', ')
-//                 }
-//               />
-//               <SHParagraph
-//                 label="Impairments"
-//                 text={hacker.application.accommodation.impairments}
-//               />
-//               <SHParagraph
-//                 label="Barriers"
-//                 text={hacker.application.accommodation.barriers}
-//               />
-//             </Flex>
-//             <hr />
-//           </SingleHackerSection>
-//           <H2 color={theme.colors.black60}>Basic Information</H2>
-//           <Flex
-//             width="100%"
-//             flexWrap="wrap"
-//             justifyContent="space-between"
-//             alignItems="center"
-//           >
-//             <SHField label="Email" text={account.email} />
-//             <SHField label="School" text={hacker.application.general.school} />
-//             <SHField label="Degree" text={hacker.application.general.degree} />
-//             <SHField label="Status" text={hacker.status} />
-//             <SHField
-//               label="Graduation Year"
-//               text={hacker.application.general.graduationYear}
-//             />
-//             <SHField
-//               label="Field(s) of Study"
-//               text={hacker.application.general.fieldOfStudy.join(', ')}
-//             />
-//             <SHField
-//               label="Skills"
-//               text={
-//                 hacker.application.shortAnswer.skills &&
-//                 hacker.application.shortAnswer.skills.join(', ')
-//               }
-//             />
-//             <SHField
-//               label="Job interest"
-//               text={hacker.application.general.jobInterest}
-//             />
-//           </Flex>
-//           <hr />
-//           <H2 color={theme.colors.black60}>Links</H2>
-//           <Flex
-//             width="100%"
-//             flexWrap="wrap"
-//             justifyContent="space-between"
-//             alignItems="center"
-//           >
-//             <SHLink
-//               label="GitHub"
-//               link={hacker.application.general.URL.github}
-//             />
-//             <SHLink
-//               label="LinkedIn"
-//               link={hacker.application.general.URL.linkedIn}
-//             />
-//             <SHLink
-//               label="Website"
-//               link={hacker.application.general.URL.other}
-//             />
-//             <SHLink
-//               label="Dribbble"
-//               link={hacker.application.general.URL.dribbble}
-//             />
-//           </Flex>
-//           {/* Only tier1 sponsors and admin have access to user resumes */}
-//           {props.userType === UserType.SPONSOR_T1 ||
-//           props.userType === UserType.STAFF ? (
-//             <Flex flexDirection={'column'} style={{ marginTop: '4em' }}>
-//               <ViewPDFComponent hackerId={hacker.id} />
-//             </Flex>
-//           ) : null}
-//           <SingleHackerSection title="Additional Information" hidden={!isAdmin}>
-//             <SHParagraph
-//               label="Why McHacks?"
-//               text={hacker.application.shortAnswer.question1}
-//             />
-//             <SHParagraph
-//               label="What are you passionate about?"
-//               text={hacker.application.shortAnswer.question2}
-//             />
-//             <SHParagraph
-//               label="Comments"
-//               text={hacker.application.shortAnswer.comments}
-//             />
-//           </SingleHackerSection>
-//         </Box>
-//       </MaxWidthBox>
-//     </article>
-//   );
-// };
-
-// export default SingleHackerView;
