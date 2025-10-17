631387e93ac0153abab2c7e1650a7d452df3a242
JAMIE XIAO <jamie.xiao.ca@gmail.com>
2025-10-17 00:37:00 -0400

    reviewer score filter

--- src/features/Search/Filters.tsx
+++ src/features/Search/Filters.tsx
@@ -19,7 +19,7 @@ import { getOptionsFromEnum } from '../../util';
 
 interface IFilterProps {
   initFilters: ISearchParameter[];
-  onChange: (newFilters: ISearchParameter[], reviewStatus?: number[]) => void;
+  onChange: (newFilters: ISearchParameter[], reviewStatus?: number[], reviewScore?: number[]) => void;
   onResetForm: () => void;
   loading: boolean;
 }
@@ -124,6 +124,23 @@ class FilterComponent extends React.Component<IFilterProps, {}> {
           value={fp.values.reviewStatus}
         />
         <FastField
+          name={'reviewScore'}
+          label={'Review Score'}
+          placeholder={'Review Scores...'}
+          isMulti={true}
+          creatable={true}
+          options={[
+            { label: '-1', value: -1 },
+            { label: '0', value: 0 },
+            { label: '1', value: 1 },
+            { label: '2', value: 2 },
+            { label: '3', value: 3 },
+            { label: '4', value: 4 },
+          ]}
+          component={FormikElements.Select}
+          value={fp.values.reviewScore}
+        />
+        <FastField
           name={'skills'}
           label={'Skills'}
           placeholder={'Skills...'}
@@ -209,7 +226,8 @@ class FilterComponent extends React.Component<IFilterProps, {}> {
       jobInterestParam
     );
     // this.props.onChange(search);
-    this.props.onChange(search, values.reviewStatus);
+    // this.props.onChange(search, values.reviewStatus);
+    this.props.onChange(search, values.reviewStatus, values.reviewScore);
   }
 
   /**
