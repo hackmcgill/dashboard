36ca34da99aee2bea11647a965b42411c612b241
JAMIE XIAO <jamie.xiao.ca@gmail.com>
2025-10-17 01:20:55 -0400

    whitelist = 5

--- src/features/Search/Filters.tsx
+++ src/features/Search/Filters.tsx
@@ -130,12 +130,13 @@ class FilterComponent extends React.Component<IFilterProps, {}> {
           isMulti={true}
           creatable={true}
           options={[
-            { label: '-1', value: -1 },
+            { label: '-1', value: -1 }, // both none
             { label: '0', value: 0 },
             { label: '1', value: 1 },
             { label: '2', value: 2 },
             { label: '3', value: 3 },
             { label: '4', value: 4 },
+            { label: '5', value: 5 }, // whitelist
           ]}
           component={FormikElements.Select}
           value={fp.values.reviewScore}
