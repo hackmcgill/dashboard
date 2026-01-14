88dcd74b8c41a25a851311303c257dc99059b484
Joshua Zhou <j.zhou11@share.epsb.ca>
2026-01-13 19:17:56 -0500

    mlh and more validation

--- src/config/index.ts
+++ src/config/index.ts
@@ -32,6 +32,7 @@ export * from './validationError';
 export * from './pageType';
 export * from './prizeCategories';
 export * from './sponsorChallenges';
+export * from './mlhChallenges';
 export * from './workshops';
 export * from './reviewers'
 export * from './prizeCategories';
