eac76e3eba56026176cedd00102efbb0c68ea498
Joshua Zhou <j.zhou11@share.epsb.ca>
2026-01-15 20:07:22 -0500

    remove tailed from sponsor chall

--- src/config/sponsorChallenges.ts
+++ src/config/sponsorChallenges.ts
@@ -3,7 +3,6 @@ export enum SponsorChallenges {
   SPONSOR_CHALLENGE_2 = 'Athena AI',
   SPONSOR_CHALLENGE_3 = 'Gumloop',
   SPONSOR_CHALLENGE_4 = 'National Bank',
-  SPONSOR_CHALLENGE_5 = "Tail'ed",
 }
 
 export default SponsorChallenges;
