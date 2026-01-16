f85cfc906dd05eb849af431e7634f8523f216a10
Joshua Zhou <j.zhou11@share.epsb.ca>
2026-01-15 19:23:53 -0500

    change sponsor challenges

--- src/config/sponsorChallenges.ts
+++ src/config/sponsorChallenges.ts
@@ -4,11 +4,6 @@ export enum SponsorChallenges {
   SPONSOR_CHALLENGE_3 = 'Gumloop',
   SPONSOR_CHALLENGE_4 = 'National Bank',
   SPONSOR_CHALLENGE_5 = "Tail'ed",
-  SPONSOR_CHALLENGE_6 = 'BassiliChat AI',
-  SPONSOR_CHALLENGE_7 = 'Dobson Center',
-  SPONSOR_CHALLENGE_8 = 'Desjardins',
-  SPONSOR_CHALLENGE_9 = 'NOVA',
-  SPONSOR_CHALLENGE_10 = 'CSUS',
 }
 
 export default SponsorChallenges;
