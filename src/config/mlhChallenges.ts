d3eb9ae304e30e824b63a5248c80b977c61f9d1c
Joshua Zhou <j.zhou11@share.epsb.ca>
2026-01-16 22:19:33 -0500

    add prizes to mlh challenges

--- src/config/mlhChallenges.ts
+++ src/config/mlhChallenges.ts
@@ -1,10 +1,10 @@
 export enum MlhChallenges {
-    MLH_CHALLENGE_1 = 'Best Use of ElevenLabs',
-    MLH_CHALLENGE_2 = 'Best Use of Gemini API',
-    MLH_CHALLENGE_3 = 'Best Use of MongoDB Atlas',
-    MLH_CHALLENGE_4 = 'Best Use of DigitalOcean',
-    MLH_CHALLENGE_5 = 'Best Use of Solana',
-    MLH_CHALLENGE_6 = 'Best Use of Auth0',
+    MLH_CHALLENGE_1 = 'Best Use of ElevenLabs - Custom Apple Airpods',
+    MLH_CHALLENGE_2 = 'Best Use of Gemini API - Wireless Headphones',
+    MLH_CHALLENGE_3 = 'Best Use of MongoDB Atlas - Google Swag Kits',
+    MLH_CHALLENGE_4 = 'Best Use of DigitalOcean - M5GO starter kit',
+    MLH_CHALLENGE_5 = 'Best Use of Solana - Retro Wireless Mouse',
+    MLH_CHALLENGE_6 = 'Best Use of Auth0 - Ledger Nano S Plus',
 }
 
 export default MlhChallenges;
